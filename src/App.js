ff502b9643adfd10935aa0bfecf5c7e300f8832c
filipdimko11 <dimkovski.filip11@gmail.com>
2023-05-06 00:42:26 +0200

    3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import { useState } from "react";
 import ReactMapGl, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 const TOKEN =
-  "pk.eyJ1Ijoiam9mZmRhbWlkbyIsImEiOiJjbGhiMGszY3UwbXdvM2NsdDByNzk0aG81In0.7iFNWjEsz_Bom1GzXb_DuA";
+  "pk.eyJ1Ijoiam9mZmRhbWlkbyIsImEiOiJjbGhiNDNxN2UwbnF1M2xzMnp3cHc0NnoxIn0.djxhmC2hkS7Y_e18lAao4A";
 
 function App() {
   const [newPlace, setNewPlace] = useState(null);
   const [viewport, setViewport] = useState({
     latitude: 39.077,
     longitude: -94.3436,
-    zoom: 3.5,
+    zoom: 4,
   });
-  const handleViewportChange = (newViewport) => {
-    setViewport(newViewport);
-  };
 
   return (
     <div
@@ -24,15 +21,10 @@ function App() {
     >
       <ReactMapGl
         {...viewport}
-        mapboxApiAccessToken={TOKEN}
+        mapboxAccessToken={TOKEN}
         width="100%"
         height="100%"
-        onViewportChange={setViewport}
-        interactive={true}
-        onDrag={handleViewportChange}
-        scrollZoom={true}
-        dragPan={true} // enable dragging
-        dragRotate={false} // disable rotation
+        transitionDuration="200"
         mapStyle="mapbox://styles/joffdamido/clhb0yvqr012801pg7snja9cz"
       >
         <>
@@ -43,178 +35,37 @@ function App() {
             offsetTop={-7 * viewport.zoom}
             color="#808080"
             style={{
-              cursor: "pointer",
+              cursor: "default",
             }}
           ></Marker>
           <Marker
-            latitude={39.077}
+            latitude={48.077}
+            longitude={-102.343}
+            offsetLeft={-0.35 * viewport.zoom}
+            offsetTop={-7 * viewport.zoom}
+            color="#808080"
+            style={{
+              cursor: "default",
+            }}
+          ></Marker>
+          <Marker
+            latitude={32.077}
             longitude={-94.343}
             offsetLeft={-0.35 * viewport.zoom}
             offsetTop={-7 * viewport.zoom}
             color="#808080"
             style={{
-              cursor: "pointer",
+              cursor: "default",
             }}
           ></Marker>
           <Marker
-            latitude={39.077}
-            longitude={-94.343}
+            latitude={40.077}
+            longitude={-120.343}
             offsetLeft={-0.35 * viewport.zoom}
             offsetTop={-7 * viewport.zoom}
             color="#808080"
             style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={39.077}
-            longitude={-94.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={45.077}
-            longitude={-104.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={44.077}
-            longitude={-72.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-
-          <Marker
-            latitude={42.077}
-            longitude={-114.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#FFFF00"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={39.077}
-            longitude={-104.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={36.077}
-            longitude={-91.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={41.077}
-            longitude={-105.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={31.077}
-            longitude={-86.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#FFFF00"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={35.077}
-            longitude={-92.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={33.077}
-            longitude={-94.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={46.466}
-            longitude={-89.7015}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={45.0356}
-            longitude={-94.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#FFFF00"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={39.0356}
-            longitude={-100.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={35.0356}
-            longitude={-105.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#808080"
-            style={{
-              cursor: "pointer",
-            }}
-          ></Marker>
-          <Marker
-            latitude={42.0356}
-            longitude={-104.343}
-            offsetLeft={-0.35 * viewport.zoom}
-            offsetTop={-7 * viewport.zoom}
-            color="#FFFF00"
-            style={{
-              cursor: "pointer",
+              cursor: "default",
             }}
           ></Marker>
         </>
